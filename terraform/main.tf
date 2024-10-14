f8d80410a486138c60762e874857405930ad2ebd
NoctDevT <korzone0@gmail.com>
2024-10-14 10:07:35 -0400

    cloudfront ssl setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,10 +39,53 @@ resource "aws_s3_bucket_policy" "website_policy" {
   })
 }
 
-
-
 resource "aws_route53_zone" "main" {
-  name = var.domain_name 
+  name = var.domain_name
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.static_website.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.static_website.id}"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.static_website.id}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "s3-oac-239292"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+  origin_access_control_origin_type = "s3"
 }
 
 resource "aws_route53_record" "www" {
@@ -50,8 +93,8 @@ resource "aws_route53_record" "www" {
   name    = var.domain_name
   type    = "A"
   alias {
-    name                   = aws_s3_bucket.static_website.website_endpoint
-    zone_id                = aws_s3_bucket.static_website.hosted_zone_id
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
 }
---
+++ terraform/ssl.tf
@@ -0,0 +1,32 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  provider          = aws.us-east-1
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
+}
+
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
